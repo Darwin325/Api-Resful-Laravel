c05034c69275ca740ccb4f8777616083ecac14cc
Darwin Zamora <darwinzc325@gmail.com>
2020-01-09 22:17:33 -0500

    Implementando el método index para SellerBuyerController

---
+++ app/Http/Controllers/Seller/SellerBuyerController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Seller;
+
+use App\Http\Controllers\ApiController;
+use App\Seller;
+
+class SellerBuyerController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Seller $seller)
+    {
+        $buyer = $seller->products()
+            ->whereHas('transactions')
+            ->with('transactions.buyer')
+            ->get()
+            ->pluck('transactions')
+            ->collapse()
+            ->pluck('buyer')
+            ->unique()
+            ->values();
+        return $this->showAll($buyer);
+    }
+}
