75b8c20f41f20f41c2b52be66e02d2e11d456908
Darwin Zamora <darwinzc325@gmail.com>
2020-01-24 14:27:22 -0500

    Enviando correos con templates markdown de laravel.

--- app/Mail/UserCreated.php
+++ app/Mail/UserCreated.php
@@ -29,6 +29,6 @@ class UserCreated extends Mailable
      */
     public function build()
     {
-        return $this->text('emails.welcome')->subject('Confirmación de correo');
+        return $this->markdown('emails.welcome')->subject('Confirmación de correo');
     }
 }
--- resources/views/emails/confirm.blade.php
+++ resources/views/emails/confirm.blade.php
@@ -1,5 +1,13 @@
-Hola {{ $user->name }}
-Has cambiado la dirección de correo. Por favor verifícala la nueva dirección
-usando el siguiente enlace:
+@component('mail::message')
+    # Hola {{ $user->name }}
 
-{{ route('verify', $user->verification_token) }}
+    Has cambiado la dirección de correo. Por favor verifícala la nueva dirección
+    usando el siguiente enlace:
+
+    @component('mail::button', ['url' => route('verify', $user->verification_token)])
+        Confirmar mi cuenta
+    @endcomponent
+
+    Thanks,<br>
+    {{ config('app.name') }}
+@endcomponent
