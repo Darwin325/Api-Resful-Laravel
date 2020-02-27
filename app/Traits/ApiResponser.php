54ded44dcad5711381463e23a89eb1cfd0c45cdd
Darwin Zamora <darwinzc325@gmail.com>
2020-02-27 14:38:46 -0500

    Ordenando listas con los indetificadores transformados

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -23,7 +23,7 @@ trait ApiResponser
 	        return $this->successResponse(['data' => $collection], $code);
         }
 	    $transformer = $collection->first()->transformer;
-        $collection = $this->sortData($collection);
+        $collection = $this->sortData($collection, $transformer);
         $collection = $this->transformData($collection, $transformer);
 		return $this->successResponse($collection, $code);
 	}
@@ -40,10 +40,10 @@ trait ApiResponser
         return $this->successResponse(['data' => $message], $code);
     }
 
-    protected function sortData(Collection $collection)
+    protected function sortData(Collection $collection, $transformer)
     {
         if (request()->has('sort_by')) {
-            $attribute = request()->sort_by;
+            $attribute = $transformer::originalAttribute(request()->sort_by);
             $collection = $collection->sortBy->{$attribute};
         }
         return $collection;
